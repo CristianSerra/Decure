d9606218c3b22f39b764dca3b2664e58cb1f2e21
Cristais Studios <cristais.soft@gmail.com>
2024-11-21 08:27:26 -0300

    robo ajustes

--- acesso.py
+++ acesso.py
@@ -38,12 +38,15 @@ def conexao():
 
 
 class InstagramBot:
-    def __init__(self,username,password):
-        self.username = username
-        self.password = password
+    def __init__(self):
+        f = open("C:\\Users\\Administrador\\Documents\\site\\Decure\\cred.txt")
+        usuario = f.readline()
+        senha = f.readline()
+        self.username = usuario
+        self.password = senha
         service = Service()
         options = webdriver.ChromeOptions()
-        self.driver = webdriver.Chrome(service=service, options=options)
+        self.driver = webdriver.Chrome(service=service, options=options, keep_alive=True)
 
     def login(self):
         driver = self.driver
@@ -53,39 +56,33 @@ class InstagramBot:
         usuario.send_keys(self.username)
         senha = driver.find_element(By.NAME,'password')
         senha.send_keys(self.password)
-        tempo(3)
+        tempo(2)
         senha.send_keys(Keys.RETURN)
-        tempo(5)
+        tempo(7)
 
     def entralink(self, url):
         self.driver.get(url)
-        tempo(3)
-        descricao = self.driver.find_element(By.XPATH,'/html/body/div[7]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/div[1]/ul/div[1]/li/div/div/div[2]/div[1]/h1/text()[1]')
-        
+        tempo(6)
+        descricao = self.driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div[1]/div[1]/section/main/div[2]/div[1]/article/div/div[2]/div/div[2]/div[1]/ul/div[1]/li/div/div/div[2]/div[1]/h1')        
         print (descricao.text)
-        tempo(15)
 
     def pegaposts(self):    
         navega = self.driver
         navega.get('https://www.instagram.com/mis_sp/')
-        tempo(5)
+        tempo(7)
         elementos = navega.find_elements(By.TAG_NAME, 'a')
         for postagem in elementos:
             post_link = postagem.get_attribute("href")
             if "instagram.com/mis_sp/p" in post_link:
                 posts.append(post_link)
-        tempo(5)
 
 #conexao()
-f = open("C:\\Users\\Administrador\\Documents\\site\\Decure\\cred.txt")
-usuario = f.readline()
-senha = f.readline()
-print(usuario)
-Bot = InstagramBot(usuario,senha)
+
+Bot = InstagramBot()
 Bot.login()
 Bot.pegaposts()
 Bot.entralink(posts[0])
-
+tempo(100)
 
 
 
