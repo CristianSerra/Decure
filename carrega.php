e31c826d566b654a7b4d9458a2e8dbed2412bff0
Cristais Studios <cristais.soft@gmail.com>
2024-11-22 21:31:46 -0300

    ajuste acesso bco

--- carrega.php
+++ carrega.php
@@ -5,6 +5,11 @@ $dbname = 'decure'; // Nome do banco de dados
 $username = 'FATEC'; // Nome de usuário do MySQL
 $password = 'Fat2023!'; // Senha do MySQL
 
+//$host = "artmap.mysql.dbaas.com.br";
+//$username = "artmap";
+//$password = "V@xd3v44#Fgdb";
+//$dbname = "artmap";
+
 $conexao = mysqli_connect($host, $username, $password, $dbname);
 $conexao->set_charset("utf8");
 
