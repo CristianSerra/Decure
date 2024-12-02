7da0c57c2c36fe53b58e62b7d6c359c575507d79
Cristais Studios <cristais.soft@gmail.com>
2024-12-02 20:30:43 -0300

    ajustes robo
    pega apenas imagem e descricao

--- acesso.py
+++ acesso.py
@@ -10,12 +10,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 
-reels = []
-posts = []
-imagens = []
-elementos = []
-dados = []
-descricao=""
+
+paginas = [ "masp", "casatriangulo", "galatea.art_" ]
+dados = { 'imagem':None, 'descricao':None }
 
 def tempo(qtoespera):
     espera=random.randrange(qtoespera-1, qtoespera+2)
@@ -31,8 +28,6 @@ def conexao():
 
     except requests.exceptions.RequestException as e:
             print(f"Ocorreu um erro: {e}")
-
-
 
 class InstagramBot:
     def __init__(self):
@@ -57,39 +52,40 @@ class InstagramBot:
         senha.send_keys(Keys.RETURN)
         tempo(8)
 
-    def entralink(self, url):
-        self.driver.get(url)
-        tempo(6)
-        global descricao
-        elemdesc = self.driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div[1]/div[1]/section/main/div[2]/div[1]/article/div/div[2]/div/div[2]/div[1]/ul/div[1]/li/div/div/div[2]/div[1]/h1')        
-        descricao = elemdesc.text
-        elemimagem = self.driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div[1]/div[1]/section/main/div[2]/div[1]/article/div/div[1]/div/div[1]/div[2]/div/div/div/ul/li[2]/div/div/div/div/div[1]/img')
-        imagem = elemimagem.get_attribute("src")
-        urllib.request.urlretrieve(imagem,"images/masp01.jpg")
-
-    def pegaposts(self):    
+    def pegaposts(self, url):    
         navega = self.driver
-        navega.get('https://www.instagram.com/masp/')
+        site="https://www.instagram.com/"+url
+        navega.get(site)
         tempo(7)
+        posts = []
+        elementos = []
         elementos = navega.find_elements(By.TAG_NAME, 'a')
         for postagem in elementos:
             post_link = postagem.get_attribute("href")
-            if "instagram.com/masp/p" in post_link:
+            poststring="instagram.com/"+url+"/p"
+            if poststring in post_link:
                 posts.append(post_link)
+        for x in range(3):
+            navega.get(posts[x])
+            tempo(10)
+            global descricao, imagem
+            elemdesc = self.driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div[1]/div[1]/section/main/div[2]/div[1]/article/div/div[2]/div/div[2]/div[1]/ul/div[1]/li/div/div/div[2]/div[1]/h1')        
+            descricao = elemdesc.text
+            elemimagem = self.driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div[1]/div[1]/section/main/div[2]/div[1]/article/div/div[1]/div/div[1]/div[2]/div/div/div/ul/li[2]/div/div/div/div/div[1]/img')
+            imagem = elemimagem.get_attribute("src")
+            urllib.request.urlretrieve(imagem,"images/"+url+"01.jpg")
+            dados['descricao']=descricao
+            dados['imagem']=imagem
+            print ("post: "+ str(x))
+            print (dados)
+            tempo(5)
+            #conexao()
+            #tempo(100)
+
 
 Bot = InstagramBot()
 Bot.login()
-Bot.pegaposts()
-Bot.entralink(posts[0])
 
-dados = { 'imagem':'masp01.jpg',
-        'descricao':None,
-        'endereco':'av paulista, 2554',
-        'contato':'joao',
-        'agendamento':'falar com severino',
-        'link':'aqui e o robot.html' }
-
-dados['descricao']=descricao
-print (dados)
-conexao()
-tempo(100)
+for x in paginas:
+    Bot.pegaposts(x)    
+    tempo(10)
