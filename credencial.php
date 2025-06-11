7386ed000bfd148eb2d9dbfc38b112038bef1c6c
cristaisstudios <43607591+CristianSerra@users.noreply.github.com>
2025-06-10 22:36:35 -0300

    Update credencial.php
    limpeza de credenciais por questão de segurança (não expor ao público)

--- credencial.php
+++ credencial.php
@@ -1,11 +1,7 @@
 <?php
-//$host = 'localhost'; // Host do MySQL
-//$dbname = 'decure'; // Nome do banco de dados
-//$username = 'FATEC'; // Nome de usuário do MySQL
-//$password = 'Fat2023!'; // Senha do MySQL
 
-$host = "artmap.mysql.dbaas.com.br";
-$username = "artmap";
-$password = "V@xd3v44#Fgdb";
-$dbname = "artmap";
+$host = "xxxxxxx.mysql.dbaas.com.br";
+$username = "xxxxxx";
+$password = "xxxxxxxxxxxxxx";
+$dbname = "xxxxxxx";
 ?>
